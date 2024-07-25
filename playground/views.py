52d1d36c61388dd1174435d2c3572faed0b08cdd
Aboubakar Garba <aboubakarfaouzi@gmail.com>
2024-07-25 09:12:26 +0100

    part1 of django is complete

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,108 @@
+from django.forms import DecimalField
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import connection
+from django.db.models import Q, F, Prefetch, Value, Func, ExpressionWrapper
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models.functions import Concat
+from store.models import Product, OrderItem, Order, Customer
+from tags.models import Tag, TaggedItem
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # #filter multiple objects using the AN operator
+    # queryset = Product.objects.filter(inventory__lt=10, inventory__gt=100)
+    # queryset = Product.objects.filter(inventory__lt=10).filter(inventory__gt=100)
+    
+    # # filter multiple objects using the OR '|' operator we can also use for the AN '&' operator if needed
+    # # to add the negation we use the '~' operator infront of the Q object like this '~Q(inventory__gt=100)'
+    # queryset = Product.objects.filter(Q(inventory__lt=10) | Q(inventory__gt=100))
+    
+    # # To compare two objects we can use the F object
+    # queryset = Product.objects.filter(inventory=F('collection__id'))
+    
+    
+    # # Method is for limiting the results
+    # product = Product.objects.all()[5:10]
+    
+    # # Selecting fields to query use the double underscore '__' to query related fields
+    # # For this method instead of getting a bunch of product instances, we get a bunch of dictionary objects
+    # product = Product.objects.values('id', 'title', 'collection__title')
+    
+    # # For this method we get a bunch of tuples
+    # product = Product.objects.values_list('id', 'title', 'collection__title')
+    
+    
+    # queryset = OrderItem.objects.values('product_id').order_by('product_id')
+    
+    # # use the distinc method to remove duplicates
+    # # queryset = OrderItem.objects.values('product_id').distinct()
+    
+    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    
+    # # we have the 'only' method to return instances of the object while the values return dictionary values
+    # queryset = Product.objects.only('id', 'title').order_by('title')
+    
+    # # we have another method called the defer which is the opposite of the 'only' method which defer the selected fields to later
+    # queryset = Product.objects.defer('description')
+
+    # # to select related objects to add another field to the query we use the select_related when we have one object with '__'
+    # queryset = Product.objects.select_related('collection__someOtherField').all()
+    
+    #     # # We use the prefetch_related when we have multiple objects
+    #     # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
+    # #     queryset = Order.objects.select_related('customer').prefetch_related(
+    # #     Prefetch('orderitem_set', queryset=OrderItem.objects.select_related('product'))
+    # # ).order_by('-placed_at')[:5]
+        
+    #     queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+
+    # # # Sometimes we wana compute summaries like max or average price of our products this is where we use cthe aggregate method
+    # # # To rename the name of the field simply give the name of the field followed by an equal sign
+    # # result = Product.objects.aggregate(max_price=Max('unit_price'), average_price=Avg('unit_price'))
+    
+    # queryset = Customer.objects.annotate(new_id=F('id')+ 1)
+    
+    # # Calling database functions
+    
+    # # Using the Func class
+    # queryset = Customer.objects.annotate(
+    #     full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+    # )
+    
+    # # Using the Concat function
+    # queryset = Customer.objects.annotate(
+    #     full_name=Concat('first_name', Value(' '), 'last_name')
+    # )
+    
+    
+    # # Working with expression wrappers, we use this when building complex expressions
+    # discounted_price = ExpressionWrapper(
+    #     F('unit_price') * 0.8, output_field=DecimalField())
+    # queryset = Product.objects.annotate(
+    #     discounted_price = discounted_price
+    # )
+    
+    # # Custom managers check the tag models to see the implementation
+    # content_type = TaggedItem.objects.get_tags_for(Product, 1)
+    
+
+    # # Executing raw sql queries. only use this method when dealing with complex queries
+    # queryset = Product.objects.raw('SELECT * FROM store_product')
+    
+    
+    # # we can also use the connection method to query data from out database
+    # cursor = connection.cursor()
+    # cursor.execute('SELECT * FROM store_product')
+    # queryset = cursor.close()
+    
+    
+    # another way is to use the with method. this way we dont have to close the cursor
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT * FROM store_product')
+        
+        
+        
+    return render(request, 'hello.html', {'name': 'Garbatov', 'result': list(queryset)})
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,130 @@
-from django.contrib import admin
+from typing import Any
+from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
+from . import models
 
-# Register your models here.
+
+
+
+# To create our own cutom filter here is how to proceed
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+    
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+        
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        
+        
+
+
+
+# admin.site.register(models.Product, ProductAdmin)
+# the below line is equivalent to the above
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Customizing forms
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    search_fields = ['title']
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    # To prevent extra queries for the collection title method we use the below
+    list_select_related = ['collection']
+    
+    
+    # if we want to access a partiular field of another table like title of the collection
+    # we can define the below method to achieve that
+    def collection_title(self, product):
+        return product.collection.title
+    
+    # to implement sorting for this method we use the below decorator
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+    
+    # Creating custom actions here request represents the HTTP request and queryset 
+    # represent the object the user has selected also to show a message to the user 
+    # we can use the method self.message_user as below
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request, 
+            f'{updated_count} products were successfully updated',
+            messages.SUCCESS
+            )
+    
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+    ordering = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        # we use this method to tell django to give us the url of a page
+        # reverse('admin:app_model_page') then to filter we gonna add a '?' 
+        url = (reverse('admin:store_product_changelist') + '?collection_id=' + str(collection.id))
+        return format_html('<a href="{}">{}<a>', url, collection.products_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count('product'))
+    
+    
+    
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'order_count']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    
+    # we can add lookup tags to specify the field queries as startswith, endswith etc
+    # To be sure to not fall in the case sensitive query specify it by starting with 'i' which stands for case insensitive
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+    
+    @admin.display(ordering='order_count')
+    def order_count(self, customer):
+        url = reverse('admin:store_order_changelist') + '?customer_id=' + str(customer.id)
+        return format_html('<a href="{}">{}<a>', url, customer.order_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(order_count=Count('order'))
+    
+    
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    model = models.OrderItem
+    min_num = 1
+    max_num = 10
+    extra = 0
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer_first_name']
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_per_page = 10
+    list_select_related = ['customer']
+    
+    def customer_first_name(self, order):
+        return order.customer.first_name
+
